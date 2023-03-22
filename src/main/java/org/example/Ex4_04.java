ce0c8200fd150a5183ef986e0480d7950c5bf226
woghdqkr <hong8011e@naver.com>
2023-03-22 12:56:59 +0900

    .

---
+++ src/main/java/org/example/Ex4_04.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Ex4_04 {
+    public static void main(String[] args){
+    int a = 10, b;
+
+    b = a++;
+    System.out.printf("%d \n", b);
+
+    b = ++a;
+    System.out.printf("%d \n",b);
+}
+}
---
+++ src/main/java/org/example/Ex4_06.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Ex4_06 {
+    public static void main(String[] args){
+    int a = 99;
+
+    System.out.printf("AND 연산 : %S \n", (a >=100) && (a<=200));
+    System.out.printf("OR 연산 : %s \n", (a >=100) || (a<=200));
+    System.out.printf("NOT 연산 : %s \n", !(a == 100));
+}
+}
