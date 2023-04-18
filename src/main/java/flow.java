6d04818b9e979865019f184751984b02cd30b559
woghdqkr <hong8011e@naver.com>
2023-04-18 10:46:50 +0900

    잔디

---
+++ src/main/java/flow.java
@@ -0,0 +1,15 @@
+public class flow {
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=5; j++){
+                if(i==j){
+                    System.out.printf("%d,%d",i,j);
+                }
+                else{
+                    System.out.printf("%c",' ');
+                }
+            }
+            System.out.println();
+        }
+    }
+}
