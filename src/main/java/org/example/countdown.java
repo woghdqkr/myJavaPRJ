503cb6d6ad35c81af45e25f52878865dd7366393
woghdqkr <hong8011e@naver.com>
2023-04-20 11:26:51 +0900

    잔디

---
+++ src/main/java/org/example/countdown.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class countdown {
+    public static void main(String[] args) {
+        int i = 11;
+
+        System.out.println("카운트 다운 시작");
+
+        while (i--!= 0){
+            System.out.println(i);
+
+            for (int j =0; j<20; j++){
+                ;
+            }
+        }
+
+        System.out.println("Game over");
+    }
+}
